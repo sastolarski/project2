5740f17e080d716168a23f12672459790db3a8f0
Jiahong Rong <jiahongrong@Jiahongs-MacBook-Pro.local>
2018-07-22 17:59:33 -0500

    fixed handlebars

--- server.js
+++ server.js
@@ -1,48 +1,84 @@
-require("dotenv").config();
+// require("dotenv").config();
+// var express = require("express");
+// var bodyParser = require("body-parser");
+// var exphbs = require("express-handlebars");
+
+// var db = require("./models");
+
+// var app = express();
+// var PORT = process.env.PORT || 3000;
+
+// // Middleware
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(express.static("public"));
+
+// // Handlebars
+// app.engine(
+//   "handlebars",
+//   exphbs({
+//     defaultLayout: "main"
+//   })
+// );
+// app.set("view engine", "handlebars");
+
+// // Routes
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
+
+// var syncOptions = { force: false };
+
+// // If running a test, set syncOptions.force to true
+// // clearing the `testdb`
+// if (process.env.NODE_ENV === "test") {
+//   syncOptions.force = true;
+// }
+
+// // Starting the server, syncing our models ------------------------------------/
+// db.sequelize.sync(syncOptions).then(function() {
+//   app.listen(PORT, function() {
+//     console.log(
+//       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+//       PORT,
+//       PORT
+//     );
+//   });
+// });
+
+// module.exports = app;
+
+
 var express = require("express");
 var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
 
-var db = require("./models");
+var PORT = process.env.PORT || 8080;
 
 var app = express();
-var PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
-
-// Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-var syncOptions = { force: false };
+// parse application/json
+app.use(bodyParser.json());
 
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+// var routes = require("./controllers/burgers_controller.js");
+
+// app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
-
-module.exports = app;
